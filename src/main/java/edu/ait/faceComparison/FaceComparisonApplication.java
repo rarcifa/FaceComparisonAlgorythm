8459169a54f14ef60f3b85e70afae4f578277c05
Rice <a00279376@student.ait.ie>
2021-03-14 19:01:38 +0100

    first commit

---
+++ src/main/java/edu/ait/faceComparison/FaceComparisonApplication.java
@@ -0,0 +1,13 @@
+package edu.ait.faceComparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceComparisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceComparisonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ait/faceComparison/FaceComparisonApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ait.faceComparison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceComparisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
